a203e8d6cb8603ba67981f944ff11560f5da7d3c
krasteki <kiril.krastev@hashicorp.com>
2022-04-11 15:25:19 +0300

    first-commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+/*
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+*/
+
+provider "cloudflare" {
+  email   = var.my_mail
+  api_token = var.my_token
+}
+
+data "cloudflare_ip_ranges" "cloudflare" {}
+
+output "source_rng" {
+    value = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
+}
